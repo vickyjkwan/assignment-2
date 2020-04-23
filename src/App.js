27160aa496cf640285616dcbffdb6fae6f7434ab
mathilda0902 <mathilda_sorciere@yahoo.com>
2020-04-23 16:16:06 -0700

    completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import ValidationComponent from './ValidationComponent/ValidationComponent';
+import CharComponent from './CharComponent/CharComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+  state = {
+    charArray: '',
+    counter: 0
+  }
+
+  counterHandler = (event) => {
+    const array = event.target.value.split('');
+    this.setState({
+      charArray: event.target.value,
+      counter: array.length
+    });
+  }
+
+  removeCharHandler = (index) => {
+    const text = this.state.charArray.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({charArray: updatedText});
+  }
+
+  render() {
+
+    const newArray = this.state.charArray.split('');
+    const charList = newArray.map((ch, index) => {
+      return <CharComponent 
+          character={ch} 
+          key={index} 
+          clicked={() => this.removeCharHandler(index)}/>
+    });
+
+    return (
+      <div className="App">
+        <input type="text" onChange={this.counterHandler} value={this.state.charArray}/>
+        <p>The character {this.state.charArray} is {this.state.counter} long.</p>
+        <ValidationComponent counter={this.state.counter}/>
+        {charList}
+      </div>
+    );
+  }
 }
 
 export default App;
